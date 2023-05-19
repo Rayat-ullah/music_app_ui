d2a3cd94cd93cd32665ddacb2df3f05e2d64531d
Rayat Ullah <rayatullah.okz@gmail.com>
2023-05-18 20:12:38 -0700

    Initial commit

---
+++ lib/providers/song_provider.dart
@@ -0,0 +1,70 @@
+
+
+import 'dart:async';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+import '../models/songs.dart';
+
+class SongProvider with ChangeNotifier {
+  SongModel? _currentSong;
+  SongModel? get currentSong => _currentSong;
+  String? lyric;
+  double? playProgress = 0, maxValue = 1;
+  set currentSong(SongModel? current) {
+    _currentSong = current;
+    getLyric(current!.lyric);
+    setAudio(current.songUrl!);
+    notifyListeners();
+  }
+
+  getLyric(String? path) async {
+    if (path != '') {
+      lyric = await rootBundle.loadString('assets/lyric/$path');
+    } else {
+      lyric = '';
+    }
+  }
+
+  bool _isPlaying = false;
+  bool get isPlaying => _isPlaying;
+  set isPlaying(bool isPlaying) {
+    _isPlaying = isPlaying;
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (isPlaying) {
+        time += 1;
+      } else {
+        timer.cancel();
+        time = 0;
+      }
+    });
+    notifyListeners();
+  }
+
+  int _time = 0;
+  int get time => _time;
+  set time(int time) {
+    _time = time;
+    notifyListeners();
+  }
+
+  final audioPlayer = AudioPlayer();
+  setAudio(String song) async {
+    audioPlayer.setReleaseMode(ReleaseMode.stop);
+    final player = AudioCache(prefix: 'assets/song/');
+    final url = await player.load(song);
+    audioPlayer.setSourceUrl(url.path,);
+    audioPlayer.play(url.path as Source);
+    audioPlayer.onPlayerStateChanged.listen((event) {
+      isPlaying = event == PlayerState.playing;
+    });
+    audioPlayer.onDurationChanged.listen((event) {
+      maxValue = event.inMilliseconds.toDouble();
+    });
+    audioPlayer.onPositionChanged.listen((event) {
+      playProgress = event.inMilliseconds.toDouble();
+    });
+  }
+}
